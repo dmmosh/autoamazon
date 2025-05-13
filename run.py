099b202afdcfe55eacbe41ec20f7fe1ffceb9dc1
dmmosh <evilsharkeater@gmail.com>
2025-05-12 22:55:04 -0400

    nothing of note

--- run.py
+++ run.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
             proxy.status = "alive"
             #print(proxy.ip)
             return
-    for i in range(0,1000):
+    for i in range(0,100):
         mock_scrape()
 
     for proxy, count in _used.most_common():
