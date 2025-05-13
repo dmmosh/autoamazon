97fccf672abe5718478e63240b42c1dab09317aa
dmmosh <evilsharkeater@gmail.com>
2025-05-12 23:34:55 -0400

    nothing of note

--- run.py
+++ run.py
@@ -112,10 +112,8 @@ if __name__ == "__main__":
         sys.exit()
     
     
-    collector = proxyscrape.create_collector('my-collector', 'http')
-
-    # Retrieve any http proxy
-    proxy = collector.get_proxy()
+    collector = proxyscrape.create_collector('default', 'http')  # Create a collector for http resources
+    proxy = collector.get_proxy({'country': 'united states'})  # Retrieve a united states proxy
     
     
     print(proxy)
