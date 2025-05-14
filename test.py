d6365b8c1445b62197360690e479fca655207df7
dmmosh <evilsharkeater@gmail.com>
2025-05-13 20:16:54 -0400

    nothing of note

--- test.py
+++ test.py
@@ -4,16 +4,33 @@ from multiprocessing.pool import Pool
 import json
 import time
 import os
+import amsin
+
+from bs4 import BeautifulSoup
+
 
 import requests
 
 # "authorization": "Basic " + os.getenv('AUTH_DECODO')
+
+
+def product_id(link:str)->str:
+    
+    _, id = link.lower().split('/dp/', 2)
+    return id[:10].upper()
+    
+
+print(product_id('www.amazon.com/DP/B01LETURZI'))
+
+
+os._exit(1)
+    
   
 url = "https://scraper-api.decodo.com/v2/scrape"
   
 payload = {
       "target": "amazon",
-      "url": "https://www.amazon.com/Unisom-Slumbers-Drug-Free-Melatonin-Raspberry/DP/B08PCCB7YS",
+      "url": "https://www.amazon.com/dp/B09H74FXNW",
       "parse": True
 }
   
@@ -28,10 +45,10 @@ response = requests.post(url, json=payload, headers=headers)
 print(response.text)
 
 
-def get_ip(i):
-    response = requests.post(url, json=payload, headers=headers)
-    r = json.loads(str(json.loads(response.text)['results'][0]['content']))['origin']
-    print(str(i) + ' ip of sender: ' + r)
+# def get_ip(i):
+#     response = requests.post(url, json=payload, headers=headers)
+#     r = json.loads(str(json.loads(response.text)['results'][0]['content']))['origin']
+#     print(str(i) + ' ip of sender: ' + r)
 
 
 # n = 20
