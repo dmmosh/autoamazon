c777f54a10a6d237efe6de2657ac4107c1b73cc9
dmmosh <evilsharkeater@gmail.com>
2025-05-15 14:14:53 -0400

    nothing of note

--- run.py
+++ run.py
@@ -50,8 +50,10 @@ def phone_num(link:str)->str: # gets the phone number from the link
 # testing purposes
 #link = 'https://www.amazon.com/dp/B08PPYQ9W5?th=1'
 def run(link:str):
+    if isinstance(link, list):
+        link = link[0]
     if(link.count('amazon.com') ==0 ): # base case
-        print(link)
+        
         print(link, 'NON-AMAZON LINK', sep='\t')
         return {}
 
